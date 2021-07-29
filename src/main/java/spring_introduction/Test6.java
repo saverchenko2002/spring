f29beb4678c71d2672199b2a1e4cc8b2b249e85a
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-07-29 20:40:20 +0300

    IOC and DI

---
+++ src/main/java/spring_introduction/Test6.java
@@ -0,0 +1,22 @@
+package spring_introduction;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Test6 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Person person = context.getBean("personBean", Person.class);
+//        person.callYourPet();
+//        Person person2 = context.getBean("personBean", Person.class);
+        System.out.println(person.getSurname() + " " + person.getAge());
+
+
+//        Pet cat1 = context.getBean("catBean", Pet.class);
+//        Pet cat2 = context.getBean("catBean", Pet.class);
+
+
+
+        context.close();
+    }
+}
